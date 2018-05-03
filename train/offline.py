527f20989c647c8581d5ae48b67c17af4207756c
Tom Alcorn <tdb.alcorn@gmail.com>
2018-05-03 13:30:25 -0700

    Implement file memory to handle large datasets on file system.

--- train/offline.py
+++ train/offline.py
@@ -1,13 +1,13 @@
 from agents.agent import Agent
 from agents import all_agents
-from train.generate import Memory
-from typing import Type
+from train.generate import Memory, Datum, Episode
+from typing import Type, List
 import tensorflow as tf
 
 
 def train(
     agent_constructor:Type[Agent],
-    memory:Memory,
+    memory:FileMemory,
     num_episodes:int,
     batch_size:int
     ):
@@ -30,6 +30,39 @@ def train(
 
         agent.save(sess)
 
+
+class FileMemory(object):
+    def __init__(self, filenames:List[str]):
+        if len(filenames) == 0:
+            raise ValueError("filenames must contain at least one filename")
+        self.filenames:List[str] = filenames
+        self.current_file_idx:int = -1
+        self.load_next()
+
+    def load_next(self):
+        '''Loads next episode into memory.'''
+        self.current_file_idx += 1
+        self.current_episode:Episode = Episode.load(self.filenames[self.current_file_idx])
+        self.current_episode_offset:int = 0
+
+    def take(self, num:int) -> List[Datum]:
+        start_idx = self.current_episode_offset
+        end_idx = self.current_episode_offset + num
+        samples = self.current_episode[start_idx:end_idx]
+        self.current_episode_offset = end_idx
+        return samples
+
+    def sample(self, batch_size:int=100, **kwargs) -> List[Datum]:
+        remainder = batch_size - (len(self.current_episode) - 1 - self.current_episode_offset)
+        if remainder <= 0:
+            return self.take(batch_size)
+        else:
+            samples = self.take(batch_size - remainder)
+            self.load_next()
+            samples.extend(self.take(remainder))
+            return samples
+
+
 if __name__ == '__main__':
     import argparse
     import sys
@@ -46,9 +79,9 @@ if __name__ == '__main__':
     if args.agent in all_agents:
         agent_constructor = all_agents[args.agent]
 
-        memory = Memory()
         filenames = glob.glob(args.data, recursive=True)
-        memory.load(filenames)
+        memory = FileMemory(filenames)
+        # memory.load(filenames)
 
         train(agent_constructor, memory, args.num_episodes, args.batch_size)
     else:
