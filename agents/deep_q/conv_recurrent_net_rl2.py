5e666eaeec6f78b1db7f593de95fefba41fbacdf
Tom Alcorn <talcorn.92@gmail.com>
2018-05-20 00:18:52 -0700

    proper rl2

--- agents/deep_q/conv_recurrent_net_rl2.py
+++ agents/deep_q/conv_recurrent_net_rl2.py
@@ -5,7 +5,7 @@ from collections import deque
 from agents.utils import flatten, find_action, as_binary_array, make_actions
 from agents.deep_q.q_net import QNet
 from agents.config import env, deep_q
-cfg = deep_q['conv_recurrent']
+cfg = deep_q['conv_recurrent_rl2']
 
 
 class FrameHistory(object):
@@ -17,12 +17,6 @@ class FrameHistory(object):
         h = list(zip(*self.buffer))
         h = [np.array(r) for r in h]
         h = [r.reshape(1, *r.shape) for r in h]
-        # for i in range(len(h)):
-        #     try:
-        #         h[i] = np.array(h[i])
-        #         h[i] = h[i].reshape(1, *h[i].shape)
-        #     except ValueError:
-        #         continue
         return h
 
     def push(self, env_state, action, target, rnn_state):
@@ -53,7 +47,8 @@ class ConvRecurrentRL2DeepQNet(QNet):
             # Inputs
             self.state = tf.placeholder(tf.float32, [None, self.num_frames, *state_shape], name='state')
             self.action = tf.placeholder(tf.int32, [None, self.num_frames, *action_shape], name='action')
-            self.target = tf.placeholder(tf.float32, [None, self.num_frames], name='target')
+            # self.target = tf.placeholder(tf.float32, [None, self.num_frames], name='target')
+            self.target = tf.placeholder(tf.float32, [None], name='target')
 
             # Dropout config
             self.training = tf.placeholder(tf.bool, name='training')
@@ -80,25 +75,38 @@ class ConvRecurrentRL2DeepQNet(QNet):
 
             # Recurrent layers
             def cell(size:int):
+                # inner = tf.nn.rnn_cell.GRUCell(size, activation=tf.nn.relu)
                 inner = tf.nn.rnn_cell.BasicLSTMCell(size)
                 return tf.nn.rnn_cell.DropoutWrapper(inner, output_keep_prob=(1-self.dropout_rate))
 
-            rnn_layers = [out_size] + cfg['rnn_layers']
-            lstm_layers = [cell(s) for s in rnn_layers]
-            self.rnn_cell = tf.nn.rnn_cell.MultiRNNCell(lstm_layers)
-            self.rnn_state = None  # initial value to be set the first time the graph is run
-            self.rnn_state_feed = self.rnn_cell.zero_state(1, tf.float32)
-            self.rnn_output, self.rnn_final_state = tf.nn.dynamic_rnn(self.rnn_cell, self.rnn_input, initial_state=self.rnn_state_feed)
-            rnn_output_shape = self.rnn_output.get_shape().as_list()
+            with tf.variable_scope("rnn"):
+                rnn_layers = [out_size] + cfg['rnn_layers']
+                cells = [cell(s) for s in rnn_layers]
+                self.rnn_cell = tf.nn.rnn_cell.MultiRNNCell(cells)
+                self.rnn_state = None  # initial value to be set the first time the graph is run
+                self.rnn_state_feed = self.rnn_cell.zero_state(1, tf.float32)
+                self.rnn_output, self.rnn_final_state = tf.nn.dynamic_rnn(self.rnn_cell, self.rnn_input, initial_state=self.rnn_state_feed)
+                rnn_output_shape = self.rnn_output.get_shape().as_list()
+                # self.rnn_output_cat = tf.reshape(self.rnn_output, (-1, *rnn_output_shape[2:]))
+                last_frame_size = reduce(lambda x,y: x*y, self.rnn_output.get_shape().as_list()[2:])
+                last_frame = tf.reshape(self.rnn_output[:, -1], (1, 1, last_frame_size))
 
+            # RL2
+            with tf.variable_scope("rl2"):
+                rl2_layers = [last_frame_size] + cfg['rl2_layers']
+                rl2_cells = [cell(s) for s in rl2_layers]
+                self.rl2_cell = tf.nn.rnn_cell.MultiRNNCell(rl2_cells)
+                self.rl2_state = None  # initial value to be set the first time the graph is run
+                self.rl2_state_feed = self.rl2_cell.zero_state(1, tf.float32)
+                self.rl2_output, self.rl2_final_state = tf.nn.dynamic_rnn(self.rl2_cell, last_frame, initial_state=self.rl2_state_feed)
+            
             # Output
-            self.rnn_output_cat = tf.reshape(self.rnn_output, (-1, *rnn_output_shape[2:]))
-            self.value_cat = tf.layers.dense(self.rnn_output_cat, self.num_actions,
+            self.value = tf.layers.dense(self.rl2_output[:, -1], self.num_actions,
                                                            activation=None)
-            self.value = tf.reshape(self.value_cat, (-1, self.num_frames, self.num_actions))
+            # self.value = tf.reshape(self.value_cat, (-1, self.num_frames, self.num_actions))
 
             # reduce_sum is now performed over axis 2
-            self.predicted_reward = tf.reduce_sum(tf.multiply(self.value, action_one_hot), axis=2)
+            self.predicted_reward = tf.reduce_sum(tf.multiply(self.value, action_one_hot[:, -1]), axis=1)
 
             # Learn
             self.loss = tf.reduce_mean(tf.square(self.target - self.predicted_reward))
@@ -107,6 +115,8 @@ class ConvRecurrentRL2DeepQNet(QNet):
     def _ensure_rnn_state(self, sess):
         if self.rnn_state is None:
             self.rnn_state = sess.run(self.rnn_state_feed)
+        if self.rl2_state is None:
+            self.rl2_state = sess.run(self.rl2_state_feed)
     
     def learn(self, sess, inp_states, inp_actions, inp_targets):
         if inp_targets is None:
@@ -116,12 +126,13 @@ class ConvRecurrentRL2DeepQNet(QNet):
         self.history.push(np.squeeze(inp_states), np.squeeze(inp_actions), np.squeeze(inp_targets), self.rnn_state)
         if len(self.history) >= self.num_frames:
             states, actions, targets = self.history.all()
-            loss, _, self.rnn_state = sess.run([self.loss, self.opt, self.rnn_final_state], feed_dict={
+            loss, _, self.rnn_state, self.rl2_state = sess.run([self.loss, self.opt, self.rnn_final_state, self.rl2_final_state], feed_dict={
                 self.state: states,
-                self.target: targets,
+                self.target: targets[-1],
                 self.action: actions,
                 self.training: True,
                 self.rnn_state_feed: self.history.rnn_states[0],
+                self.rl2_state_feed: self.rl2_state,
             })
             self.history.rnn_states[-1] = self.rnn_state
         else:
@@ -139,10 +150,13 @@ class ConvRecurrentRL2DeepQNet(QNet):
                 self.state: states,
                 self.training: False,
                 self.rnn_state_feed: self.history.rnn_states[0],
+                self.rl2_state_feed: self.rl2_state,
             })
-            action = np.argmax(np.squeeze(value)[-1])
+            action = np.argmax(np.squeeze(value))
         else:
             action = 0
+        print("\033[K", end='\r')
+        print('\r' + repr(action), end='')
         action = self.actions[action]
         return action
     
@@ -155,6 +169,7 @@ class ConvRecurrentRL2DeepQNet(QNet):
                 self.state: [nexts],
                 self.training: False,
                 self.rnn_state_feed: self.history.rnn_states[0],
+                self.rl2_state_feed: self.rl2_state,
             })
             try:
                 next_values[episode_ends] = np.zeros(next_values.shape[1:])
@@ -162,6 +177,6 @@ class ConvRecurrentRL2DeepQNet(QNet):
                 print(next_values.shape)
                 print(len(episode_ends))
                 raise
-            targets = rewards + gamma * np.max(next_values, axis=2)[:, -1]
+            targets = rewards + gamma * np.max(next_values, axis=1)
             return targets
         return None
