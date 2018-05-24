235881bf869cd8fe7e470e560c297f7cf8a9514c
Tom Alcorn <talcorn.92@gmail.com>
2018-05-24 09:55:26 -0700

    it is surprisingly complicated to serve static content with tornado

--- web/server.py
+++ web/server.py
@@ -1,16 +1,31 @@
-import tornado.ioloop
-import tornado.web
+import os
+import __main__
+import tornado.ioloop as ioloop
+import tornado.web as web
 
-class MainHandler(tornado.web.RequestHandler):
+class IndexHandler(web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.render('index.html')
+        # self.write("Hello, world")
 
 def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+    if hasattr(__main__, '__file__'):
+        cwd = os.path.abspath(os.path.join(os.path.dirname(__main__.__file__), '..'))
+    else:
+        # assume being called from directory above web
+        cwd = os.path.abspath(os.path.join(os.getcwd(), '..'))
+    static_dir = os.path.join(cwd, 'web/static')
+    return web.Application([
+        (r"/", IndexHandler),
+        (r"/static/(.*)", web.StaticFileHandler, {'path': static_dir, }),
+    ],
+    # static_path=static_dir,
+    )
+    # # static_prefix='',
+    # static_file_args=dict(default_filename='./index.html'))
 
 if __name__ == "__main__":
     app = make_app()
+    # print(app.settings['static_path'])
     app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    ioloop.IOLoop.current().start()
